98da24ac6d0299d6420f085b767e717d1ed8bd0d
ZeronX <140531897+zeperix@users.noreply.github.com>
2025-02-02 19:24:04 +0700

    up

--- shell.sh
+++ shell.sh
@@ -7,6 +7,6 @@ cat <<EOF >> ~/.bashrc
 PS1="\n\[\033[0;31m\]┌─[\[\033[1;34m\]\u\[\033[1;33m\]@\[\033[1;36m\]localhost\[\033[0;31m\]]─[\[\033[0;32m\]\w\[\033[0;31m\]]\n\[\033[0;31m\]└──╼ \[\e[1;31m\]❯\[\e[1;33m\]❯\[\e[1;32m\]❯\[\033[0m\] "
 EOF
 
-source .bashrc
 
-echo "PS1 đã được cập nhật!"
+
+echo "PS1 đã được cập nhật!, hãy chạy source ~/.bashrc"
